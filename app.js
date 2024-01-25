3d10091ce8215519da8afc8d990b9d263faed282
cleberfarias <cleber_afd@hotmail.com>
2024-01-24 22:25:12 -0300

    Uso do Alert e condições if

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Bem vindo ao jogo de adivinhação do número secreto');
+let numeroSecreto = 29;
+let chute = prompt('Escola o numero entre 1 e 30');
+
+if(chute == numeroSecreto){
+    console.log('Isso ai! Você acertou o número secreto(5)');
+}
